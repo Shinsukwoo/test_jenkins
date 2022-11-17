02e1a4bbabc57cf508d62dfbe35a71873eb349de
dlsghzx998 <dlsghzx998@gmail.com>
2022-11-17 02:47:23 +0000

    Seok add model

--- myweb/myweb/settings.py
+++ myweb/myweb/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '20221111',
+        'HOST': 'test-dbeaver1.cuigmsrbttex.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',        
     }
 }
 
@@ -106,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
